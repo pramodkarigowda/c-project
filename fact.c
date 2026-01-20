3aaf87b582b6c2ab2dec16c77b8c3f84cc1b8b6b
Cloud User <ec2-user@ip-172-31-12-61.ap-south-1.compute.internal>
2026-01-20 13:06:47 +0000

    adding c files

---
+++ fact.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    unsigned long long fact = 1;  // factorial grows fast, so use long long
+
+    printf("Enter a positive integer: ");
+    scanf("%d", &n);
+
+    if (n < 0) {
+        printf("Factorial of a negative number doesn't exist.\n");
+    } else {
+        for (i = 1; i <= n; i++) {
+            fact *= i;
+        }
+        printf("Factorial of %d = %llu\n", n, fact);
+    }
+
+    return 0;
+}
+
