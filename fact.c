038a329eb378544bcfa84eb7d1f6a8efde550acc
pramodkarigowda <pramodgowdak22@gmail.com>
2026-01-27 18:36:37 +0530

    Update fact.c

--- fact.c
+++ fact.c
@@ -2,12 +2,12 @@
 int factorial() {
     int n, i;
     unsigned long long fact = 1;
-    printf("Enter an integer: ");
+    printf("\nEnter an integer: ");
     scanf("%d", &n);
 
     // shows error if the user enters a negative integer
     if (n < 0)
-        printf("Error! Factorial of a negative number doesn't exist.");
+        printf("\nError! Factorial of a negative number doesn't exist.");
     else {
         for (i = 1; i <= n; ++i) {
             fact *= i;
